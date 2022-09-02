d8163521bc9fe344ea0c29e1be06b6eb52f12e21
gitmehrab <hasanmehrab224@gmail.com>
2022-09-03 05:26:26 +0600

    click display non-dynamic add

--- main.js
+++ main.js
@@ -9,20 +9,57 @@ const displayNewsCatagory = items =>{
     const listItem = document.getElementById('list-menu');
     
      for(const item of items){
-      //  console.log(item.category_name)
+      
         const createList = document.createElement('div');
-        
-        
-       // createList.classList.add('card')
         createList.innerHTML = `
-        <a id="${item.category_id}" href="" class="text-decoration-none text-secondary" onclick="displayCatagoryId()"> ${item.category_name}</a>
+        <button id="${item.category_id}" onclick="loadCatagoryId('${item.category_id}')"  class="bg-white border-0 text-secondary" > ${item.category_name}</button>
         `;
         listItem.appendChild(createList);
     } 
     
 }
 
+const loadCatagoryId = (category_id) =>{
+    //console.log(category_id)
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+    .then(res => res.json())
+    .then(data => displayCatagoryId(data.data));
+}
 
+
+    
+    const displayCatagoryId = idList =>{
+        const idListDiv = document.getElementById('idList')
+       for(id of idList){
+        //console.log(id)
+        const createIdDiv = document.createElement('div');
+        createIdDiv.classList.add('d-flex','catagory', 'justify-content-between', 'border','my-4')
+        createIdDiv.innerHTML = `
+        <div class="col-4">
+              <img class="img-fluid" style="height:100%; width:100%" src="${id.thumbnail_url}" alt="">
+            </div>
+            <div class="col-7">
+              <h4>${id.title}</h4>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia modi ex sint necessitatibus alias nobis dignissimos obcaecati illo nesciunt doloribus, est blanditiis reiciendis quae pariatur praesentium quasi odit minima veritatis.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="">
+                <img class="img-fluid" style="height:50px" src="${id.author.img}" alt="">
+                <p>${id.author.published_date}</p>
+                </div>
+                <div class="d-flex gap-2 justify-content-center">
+                  <i class="fa-regular fa-eye mt-1"></i>
+                  <p>${id.total_view}</p>
+                </div>
+                <i class="fa-solid fa-right-long"></i>
+              </div>
+            </div>
+        `
+        idListDiv.appendChild(createIdDiv)
+       }
+    }
+    
+    loadCatagoryId()
 
 
 loadNewsCatagory() 
+//onclick="loadCatagoryId('${item.category_name}')"
